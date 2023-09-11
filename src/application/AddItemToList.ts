5480c89f82261591c4cea2d293d7dd12dc624b3e
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-09-11 20:08:37 +0200

    add mapping and presentable types

--- src/application/AddItemToList.ts
+++ src/application/AddItemToList.ts
@@ -2,18 +2,23 @@ import { AbstractUseCase } from './AbstractUseCase.ts';
 import { Item } from '../domains/Item.ts';
 import { List } from '../domains/List.ts';
 
+export type PresentableListItem = {
+  id: string;
+  title: string;
+  status: boolean;
+};
+
+export type PresentableList = PresentableListItem[];
+
 export interface ListStorage {
   load(): List;
-
   store(list: List): List;
 }
 
 export interface ListPresenter {
   startLoading(): void;
-
   finishLoading(): void;
-
-  displayList(list: List): void;
+  displayList(list: PresentableList): void;
 }
 
 export interface AddItemToListAdapter {
@@ -22,19 +27,23 @@ export interface AddItemToListAdapter {
 }
 
 export class AddItemToList extends AbstractUseCase<{ item: Item }, void> {
-  // just Item??
-
   constructor(private readonly _adapters: AddItemToListAdapter) {
     super();
   }
 
-  execute({ item }: { item: Item }): Promise<void> | void {
+  execute({ item }: { item: Item }): void {
     this._adapters.listPresenter.startLoading();
     const list = this._adapters.listStorage.load();
     list.addItem(item);
-    const newList = this._adapters.listStorage.store(list);
-    console.log(newList);
-    this._adapters.listPresenter.displayList(newList);
+    this._adapters.listStorage.store(list);
+    const presentbaleList = list.items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        status: item.status,
+      };
+    });
+    this._adapters.listPresenter.displayList(presentbaleList);
     this._adapters.listPresenter.finishLoading();
   }
 }
