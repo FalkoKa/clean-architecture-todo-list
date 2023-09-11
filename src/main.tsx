e80314a7bde6599a4725dac33fc1b8dda85682fd
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-09-11 19:44:19 +0200

    change context and pass viewmodel as props

--- src/main.tsx
+++ src/main.tsx
@@ -14,14 +14,14 @@ import {
 } from './adapters/Presenter/ViewModel.ts';
 
 type State = {
-  listContext?: ViewModel;
+  viewModel?: ViewModel;
 };
 
 type Props = {};
 
 export class Main extends React.PureComponent<Props, State> {
   private readonly _controller: Controller;
-  private _adapters: AddItemToListAdapter;
+  private readonly _adapters: AddItemToListAdapter;
   private readonly _useCases: UseCases;
 
   constructor(props: Props) {
@@ -45,20 +45,15 @@ export class Main extends React.PureComponent<Props, State> {
   }
 
   public renderListPage(viewModel: ViewModel): void {
-    this.setState({
-      ...this.state,
-      listContext: viewModel,
-    });
+    this.setState({ viewModel });
   }
 
   render(): React.ReactElement {
-    const { listContext } = this.state;
+    const { viewModel } = this.state;
     return (
       <>
-        <Provider
-          value={{ controller: this._controller, viewModel: listContext }}
-        >
-          <ListPage />
+        <Provider value={{ controller: this._controller }}>
+          {viewModel && <ListPage viewModel={viewModel} />}
         </Provider>
       </>
     );
