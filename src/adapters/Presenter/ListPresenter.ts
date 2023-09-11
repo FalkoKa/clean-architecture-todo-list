e2ca3af6509057f998b516be63517bddc745eafc
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-09-11 20:09:13 +0200

    fix set state bug

--- src/adapters/Presenter/ListPresenter.ts
+++ src/adapters/Presenter/ListPresenter.ts
@@ -1,30 +1,33 @@
-import {ListPresenter} from "../../application/AddItemToList.ts";
-import {List} from "../../domains/List.ts";
-import {createInitialViewModel, ViewModel} from "./ViewModel.ts";
+import {
+  ListPresenter,
+  PresentableList,
+} from '../../application/AddItemToList.ts';
+import { createInitialViewModel, ViewModel } from './ViewModel.ts';
 
 export interface RendersListPage {
-    renderListPage(viewModel: ViewModel): void
+  renderListPage(viewModel: ViewModel): void;
 }
 
 export class Presenter implements ListPresenter {
-    private _state: ViewModel = createInitialViewModel()
+  private _state: ViewModel = createInitialViewModel();
 
-    constructor(private readonly _renderer: RendersListPage) {
-    }
+  constructor(private readonly _renderer: RendersListPage) {}
 
-    displayList(list: List): void {
-        this._state.list = list.items
-        console.log(this._state.list)
-        this._renderer.renderListPage(this._state)
-    }
+  displayList(list: PresentableList): void {
+    this._state = {
+      ...this._state,
+      list,
+    };
+    this._renderer.renderListPage(this._state);
+  }
 
-    startLoading(): void {
-        this._state.global.isLoading = true
-        this._renderer.renderListPage(this._state)
-    }
+  startLoading(): void {
+    this._state.global.isLoading = true;
+    this._renderer.renderListPage(this._state);
+  }
 
-    finishLoading(): void {
-        this._state.global.isLoading = false
-        this._renderer.renderListPage(this._state)
-    }
+  finishLoading(): void {
+    this._state.global.isLoading = false;
+    this._renderer.renderListPage(this._state);
+  }
 }
