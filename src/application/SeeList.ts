14964fdd53bc8b9e427f69ebbc71522e53f91a2d
FalkoKa <115454465+FalkoKa@users.noreply.github.com>
2023-09-13 20:53:00 +0200

    delete file

--- src/application/SeeList.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractUseCase } from './AbstractUseCase.ts';
-
-export class SeeList extends AbstractUseCase {
-  execute(input: void | undefined): Promise<void> | void {
-    return undefined;
-  }
-}
